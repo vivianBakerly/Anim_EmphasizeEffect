d61c3601d28cd56043f7311faac804c1f6315ca6
isabellaHea <isabella1217@163.com>
2016-03-02 11:32:13 +0800

    Initial Commit

---
+++ isa_EmphasizeEffect/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  isa_EmphasizeEffect
+//
+//  Created by isahuang on 16/3/2.
+//  Copyright © 2016年 isahuang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ isa_EmphasizeEffect/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  isa_EmphasizeEffect
+//
+//  Created by isahuang on 16/3/2.
+//  Copyright © 2016年 isahuang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
